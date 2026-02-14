0a95bc70768b10d40fe82b5411c2e92199105935
maharufh <araju8391@gmail.com>
2026-02-15 01:13:47 +0530

    checkin app

--- React/viteapp/src/App.jsx
+++ React/viteapp/src/App.jsx
@@ -7,11 +7,15 @@ function App() {
  
 
   return (
+
+
     // <UserContext.Provider value={user}>
     //   <h2> i m component one so hello {user}</h2>
     //   <Component3 />
     // </UserContext.Provider>
     <>
+
+    <h2> I am app compo</h2>
     <Timer/>
     </>
   );
